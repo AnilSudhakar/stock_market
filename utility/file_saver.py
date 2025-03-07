fcb4f69b84c5f4333de79a091c9beebfd7ee67be
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-03-07 13:35:48 +0100

    updated the metgod

--- utility/file_saver.py
+++ utility/file_saver.py
@@ -6,9 +6,13 @@ class FileSaver:
         self.output_folder = 'output'
         os.makedirs(self.output_folder, exist_ok=True)
 
-    def save_to_file(self,  content, file_name):
+    def save_to_file(self, symbol, content, file_name):
         file_name = file_name.replace(' ', '_').lower()
-        file_path = os.path.join(self.output_folder, file_name)
+
+        folder_path = os.path.join(self.output_folder, symbol)
+        os.makedirs(folder_path, exist_ok=True)
+
+        file_path = os.path.join(folder_path, file_name)
         print(f'Saving to file: {file_path}')
         with open(file_path, 'w') as file:
             for line in content:
